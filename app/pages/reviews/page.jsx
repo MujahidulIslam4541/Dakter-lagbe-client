b4987e2b5dfec79ace3f0fda6f176dbdc9d16ab8
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-11-05 15:40:37 +0600

    create our service structure

--- app/pages/reviews/page.jsx
+++ app/pages/reviews/page.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 const OurReviews = () => {
   return (
@@ -11,6 +21,27 @@ const OurReviews = () => {
           aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
         </p>
       </div>
+
+      {/* slider */}
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="">Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
