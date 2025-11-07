d4c61fc502eae9f619004bdd7ba77b6d0c3aa2e6
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-11-07 22:59:25 +0600

    some  changes

--- app/components/navbar/page.jsx
+++ app/components/navbar/page.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-[#07332F]  fixed top-0 left-0 w-full z-50 text-white ">
-      <div className="max-w-7xl mx-auto flex items-center justify-between lg:px-32 px-6 py-3">
+      <div className="max-w-7xl mx-auto flex items-center justify-between  overflow-hidden px-6 py-3">
         {/* Logo + Brand Name */}
         <div className="flex items-center gap-2">
 
